f1af1a9fc8020aa5abc58bcfefe753d2ee0ba00d
r7sysytem <teacherfatma29@gmail.com>
2026-05-01 21:37:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("R7 BOT WORKING 🔥");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
